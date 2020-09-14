2f744fdac6571d6322de373ec5815fe2b912ccd3
alexmr1 <alexmrobu@gmail.com>
2020-09-14 14:29:58 +0100

    ProjList component created

--- src/components/ProjList.jsx
+++ src/components/ProjList.jsx
@@ -3,14 +3,63 @@ import React, { Component } from "react";
 class ProjList extends Component {
   render() {
     return (
-      <div>
+      <div className="projectsList">
         <ul>
-          <h3>
-            <li>Project 1</li>
-            <li> Project 2</li>
-            <li> Project 3</li>
-            <li> Project 4</li>
-          </h3>
+          {/* map over the projects */}
+          <li className="indvProject">
+            <h5> Proj 1 - General Project Description</h5>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minus
+              quibusdam molestiae adipisci nulla dignissimos non! Dicta, aliquid
+              autem. Odit quasi non quod fuga, culpa illo voluptatum quos fugiat
+              veritatis.{" "}
+            </p>
+            <h5> Proj 1 - Individual Project Contribution</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
+              exercitationem unde velit quaerat, ut odio ex labore atque
+              nesciunt saepe ipsam, corporis, rerum consectetur magnam
+              dignissimos aut quod dicta tempore!
+            </p>
+            <button>Update project!</button> <button> Show/Hide</button>
+          </li>
+          <li className="indvProject">
+            <h5> Proj 2 - General Project Description</h5>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minus
+              quibusdam molestiae adipisci nulla dignissimos non! Dicta, aliquid
+              autem. Odit quasi non quod fuga, culpa illo voluptatum quos fugiat
+              veritatis.{" "}
+            </p>
+            <h5> Proj 2 - Individual Project Contribution</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
+              exercitationem unde velit quaerat, ut odio ex labore atque
+              nesciunt saepe ipsam, corporis, rerum consectetur magnam
+              dignissimos aut quod dicta tempore!
+            </p>
+            <button>Update project!</button> <button> Show/Hide</button>
+          </li>
+          <li className="indvProject">
+            <h5> Proj 3 - General Project Description</h5>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel minus
+              quibusdam molestiae adipisci nulla dignissimos non! Dicta, aliquid
+              autem. Odit quasi non quod fuga, culpa illo voluptatum quos fugiat
+              veritatis.{" "}
+            </p>
+            <h5> Proj 3 - Individual Project Contribution</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
+              exercitationem unde velit quaerat, ut odio ex labore atque
+              nesciunt saepe ipsam, corporis, rerum consectetur magnam
+              dignissimos aut quod dicta tempore!
+            </p>
+            <button>Update project!</button> <button> Show/Hide</button>
+          </li>
         </ul>
       </div>
     );
