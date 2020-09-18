edf87f24e7ef22f60c1b46b660251d1faff4294c
joaojesus81 <joao.jesus@arup.com>
2020-09-18 09:02:28 +0100

    image functionality removed

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, useField, Form, setFieldValue } from "formik";
+import { Formik, useField, Form } from "formik";
 import { observer } from "mobx-react";
 import * as Yup from "yup";
 
@@ -16,52 +16,18 @@ const CustomTextInput = ({ label, ...props }) => {
 
 const Photo = (props) => {
   const { StaffName, imgUrl, StaffID } = props.currentUser.currentUser[0];
-
   return (
     <>
       <div className="userPhoto">
         <h3>{StaffName}</h3>
-        {imgUrl ? (
-          <img src="imgUrl" alt="staff" style={{ height: 200 }} />
-        ) : (
-          <img
-            src="https://wolper.com.au/wp-content/uploads/2017/10/image-placeholder.jpg"
-            alt="placeholder"
-            style={{ height: 200 }}
-          />
-        )}
+
+        <img
+          src="https://wolper.com.au/wp-content/uploads/2017/10/image-placeholder.jpg"
+          alt="placeholder"
+          style={{ height: 200 }}
+        />
+
         <br />
-        <Formik
-          initialValue={{ imgUrl: "" }}
-          validationSchema={Yup.object({
-            imgUrl: Yup.string().required("Required"),
-          })}
-          onSubmit={(values, { setSubmitting, resetForm }) => {
-            props.currentUser.editUserMetaData(StaffID, values);
-            setSubmitting(false);
-            resetForm();
-          }}
-        >
-          {(props) => (
-            <Form>
-              <CustomTextInput
-                label="imgUrl"
-                type="text"
-                name="imgUrl"
-                placeholder="Insert image url"
-                // onChange={(event) =>
-                //   formProps.setFieldValue(
-                //     "imgUrl",
-                //     event.currentTarget.files[0]
-                //   )
-                // }
-              />
-              <button type="submit">
-                {props.isSubmitting ? "Loading..." : "Submit"}
-              </button>
-            </Form>
-          )}
-        </Formik>
       </div>
     </>
   );
