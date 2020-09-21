e6cd33d44a04f1f7c3f400e521b4496c45575632
joaojesus81 <joao.jesus@arup.com>
2020-09-21 10:48:48 +0100

    professional information fully functional and editable

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,60 +1,113 @@
-import React from "react";
-import { Formik, useField, Form } from "formik";
+import React, { Component } from "react";
+
+// import of mobx-state-tree utilities
 import { observer } from "mobx-react";
-import * as Yup from "yup";
 
-const CustomTextInput = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <label htmlFor={props.id || props.name}> {label}</label>
-      <input type="text" {...field} {...props} />
-      {meta.touched && meta.error ? <div>{meta.error}</div> : null}
-    </>
-  );
-};
-
-const Photo = (props) => {
-  const { StaffName, imgURL, StaffID } = props.currentUser.currentUser[0];
-
-  let selectedPhoto = "";
-
-  const handleChange = (event) => {
-    selectedPhoto = event.target.files[0];
+class Photo extends Component {
+  state = {
+    selectedPhoto: null,
+    isUploading: false,
+    isEditing: false,
   };
 
-  const handleClick = (event) => {
+  handleSubmit = (event, StaffID) => {
     event.preventDefault();
-    console.log(selectedPhoto);
+    this.setState({ isUploading: true });
     const fileImg = new FormData();
-    fileImg.append("image", selectedPhoto, selectedPhoto.name);
-    props.currentUser.postUserImage(StaffID, fileImg).then(() => {
+    fileImg.append(
+      "image",
+      this.state.selectedPhoto,
+      this.state.selectedPhoto.name
+    );
+    this.props.currentUser.postUserImage(StaffID, fileImg).then(() => {
       console.log("File uploaded");
+      this.setState({ isUploading: false, isEditing: false });
     });
   };
-  return (
-    <>
-      <div className="userPhoto">
-        <h3>{StaffName}</h3>
-        {imgURL === "" ? (
-          <img
-            src="https://wolper.com.au/wp-content/uploads/2017/10/image-placeholder.jpg"
-            alt="placeholder"
-            style={{ height: 200 }}
-          />
-        ) : (
-          <img src={imgURL} alt={StaffName} style={{ height: 200 }} />
-        )}
-        <form className="form" id="photo">
-          <input type="file" id="imgURL" onChange={handleChange} />
-          <button type="submit" onClick={handleClick}>
-            Upload Image
-          </button>
-        </form>
-        <br />
-      </div>
-    </>
-  );
-};
+
+  render() {
+    const {
+      StaffName,
+      imgURL,
+      StaffID,
+    } = this.props.currentUser.currentUser[0];
+    return (
+      <>
+        <div className="userPhoto">
+          <h3>
+            {StaffName}{" "}
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                this.setState({ isEditing: true });
+              }}
+            >
+              <span role="img" aria-label="edit image">
+                📝
+              </span>
+            </button>
+            {this.state.isEditing && (
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  this.setState({ isEditing: false });
+                }}
+              >
+                <span role="img" aria-label="cancel edit">
+                  ❌
+                </span>
+              </button>
+            )}
+          </h3>
+          {imgURL === null ? (
+            <img
+              src="https://wolper.com.au/wp-content/uploads/2017/10/image-placeholder.jpg"
+              alt="placeholder"
+              style={{ maxheight: 200 }}
+            />
+          ) : (
+            <img src={imgURL} alt={StaffName} style={{ maxheight: 200 }} />
+          )}
+          {this.state.isEditing && (
+            <form
+              onSubmit={(event) => this.handleSubmit(event, StaffID)}
+              className="form"
+              id="photo"
+            >
+              {!this.state.isUploading && (
+                <div>
+                  <input
+                    ref={(fileInput) => {
+                      this.fileInput = fileInput;
+                    }}
+                    style={{ display: "none" }}
+                    type="file"
+                    id="imgURL"
+                    onChange={(event) => {
+                      const obj = event.target.files[0];
+                      this.setState({ selectedPhoto: obj });
+                    }}
+                  />
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      this.fileInput.click();
+                    }}
+                  >
+                    Select a photo
+                  </button>
+                </div>
+              )}
+              <button type="submit">
+                {this.state.isUploading ? "Uploading..." : "Upload Image"}
+              </button>
+            </form>
+          )}
+          <br />
+        </div>
+      </>
+    );
+  }
+}
 
 export default observer(Photo);
