b589d1b5c79dd4430f17583d932e4be489a27569
alexmr1 <alexmrobu@gmail.com>
2020-09-22 09:31:32 +0100

    forms added

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,18 +1,5 @@
 import React from "react";
-import { Formik, useField, Form } from "formik";
 import { observer } from "mobx-react";
-import * as Yup from "yup";
-
-const CustomTextInput = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <label htmlFor={props.id || props.name}> {label}</label>
-      <input type="text" {...field} {...props} />
-      {meta.touched && meta.error ? <div>{meta.error}</div> : null}
-    </>
-  );
-};
 
 const Photo = (props) => {
   const { StaffName, imgURL, StaffID } = props.currentUser.currentUser[0];
