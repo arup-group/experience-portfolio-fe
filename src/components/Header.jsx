01b84317340b6368e02d84e4249518353fd3a715
joaojesus81 <joao.jesus@arup.com>
2020-09-24 11:47:21 +0100

    arup logo now links to main page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-// import Slider from "./Slider";
 import UserCard from "./UserCard";
-import { observer } from "mobx-react";
 import Login from "./Login";
+
+// import utilities from rach router
+import { Link } from "@reach/router";
+// import of mobx components
+import { observer } from "mobx-react";
 
 function Header(props) {
   return (
     <header style={{ display: "flex" }}>
-      <img
-        src="https://res-2.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1423312535/pg8bkx7fn67810xun2yp.png"
-        alt="placeholder"
-        style={{ height: 200 }}
-      />
+      <Link to="/">
+        <img
+          src="https://res-2.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1423312535/pg8bkx7fn67810xun2yp.png"
+          alt="placeholder"
+          style={{ height: 200 }}
+        />
+      </Link>
       {props.currentUser.currentUser.length !== 0 ? (
         <UserCard
           currentUser={props.currentUser}
